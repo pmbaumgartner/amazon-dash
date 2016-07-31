7f587a6c8e27b1c37bf5acacb27f609e74fde629
pmbaumgartner <peterb518@gmail.com>
2016-07-31 12:00:08 -0400

    tabs vs spaces

--- sniff.py
+++ sniff.py
@@ -8,15 +8,16 @@ db = client['dog-dash']
 
 coll = db['records_collection']
 
+
 def arp_display(pkt):
-  if pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].hwsrc == '44:65:0d:a6:17:55': #plum Organics
+  if pkt[ARP].op == 1:  # who-has (request)
+    if pkt[ARP].hwsrc == '44:65:0d:a6:17:55':  # plum Organics
         coll.insert_one(
             {
                 'dog': 'BUTTON',
                 'time': datetime.datetime.now()
             }
         )
-      print ("Plum Organics detected")
+        print ("Plum Organics detected")
 
 sniff(prn=arp_display, filter="arp", store=0, count=0)
