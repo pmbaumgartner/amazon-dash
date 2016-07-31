f9b12bb33ff3cefb87adacb9ce3aa5b98abd0ebb
pmbaumgartner <peterb518@gmail.com>
2016-07-31 12:55:25 -0400

    timing

--- sniff.py
+++ sniff.py
@@ -10,21 +10,21 @@ db = client['dog-dash']
 
 coll = db['records_collection']
 
+capture_time = time()
 
 def arp_display(pkt):
-    start_time = time()
-    if pkt[ARP].op == 1:  # who-has (request)
+    if pkt[ARP].op == 1:
         if pkt[ARP].hwsrc == '44:65:0d:a6:17:55':
-            capture_time = time()
-            if capture_time - start_time > 3:
-                time_now =  datetime.datetime.now() # plum Organics
+            if time() - capture_time > 3:
+                time_now = datetime.datetime.now() # plum Organics
                 coll.insert_one(
                     {
                         'dog': 'BUTTON',
                         'time': time_now
                     }
                 )
-                print("Plum Organics detected", str(time_now))
+                print("Plum Organics detected")
 
+                capture_time = time()
 
 sniff(prn=arp_display, filter="arp", store=0, count=0)
